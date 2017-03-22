b55fccb1896557d982e0940f1fd9e66e9ec1d0c1
vagrant <vagrant@vagrant.vm>
2017-03-22 16:49:48 +0000

    added login logout and register routes and templates

--- server.py
+++ server.py
@@ -40,26 +40,62 @@ def register_form():
 
     return render_template('registration_form.html')
 
+@app.route("/log_out", methods=['GET'])
+def log_out():
+    """logs user out."""
+    del session["logged_in"]
+    flash("Logged out")
+    return render_template('homepage.html')
 
-@app.route("/login-form")
+@app.route("/register", methods=['POST'])
+def register_process():
+    """Process the form."""
+    email = request.form.get('uemail')
+    password = request.form.get('psw')
+
+    user = User.query.filter(User.email == email).first()
+
+    if user is None:
+        
+        flash("You've created your account!")
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        session['logged_in'] = new_user.user_id
+
+        return redirect("/")
+    else:
+        flash("Email has been registered. Please use another one.")
+        return render_template("registration_form.html")
+
+
+@app.route("/login_form")
 def show_form():
     """Login Form."""
 
     return render_template("login_form.html")
 
 
-@app.route("/login-form", methods=['POST'])
+@app.route("/login_form", methods=['POST'])
 def process_form():
     """Checks if email and password match."""
 
     email = request.form.get('uemail')
-    password = request.form.get('pwd')
+    password = request.form.get('psw')
 
-    user_email = User.query.filter_by(User.email == email).first()
+    user = User.query.filter(User.email == email).first()
 
-    if user_email
+    if user is None:
+        flash("Email doesn't exist. Please log in again.")
+        return render_template("login_form.html")
+    elif user.password != password:
+        flash("Wrong password. Please type in your password again.")
+        return render_template("login_form.html")
+    else:
+        session['logged_in'] = user.user_id
+        return redirect("/")
 
-    return render_template("login_form.html")
+
 
 
 
